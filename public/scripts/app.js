b98e0824fea8e93bcd63d0bf73dba2205e02c1ae
Tyler <tyler.tomczyk@gmail.com>
2019-02-21 03:15:29 +0000

    built function redner tweets to loop through tweet array and stage to append onto tweet-container

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,24 +5,56 @@
  */
 
  $('document').ready(function(e){
-    const tweetData = {
-        "user": {
-          "name": "Newton",
-          "avatars": {
-            "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-            "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-            "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-          },
-          "handle": "@SirIsaac"
-        },
-        "content": {
-          "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-    }
     
-    //mentor said that given I was behind, ok to use this simpler method of vs. jQuery .add etc.  
-    //this function extractsdata from a tweet object, and applies into html container 
+    const data = [
+        {
+          "user": {
+            "name": "Newton",
+            "avatars": {
+              "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+              "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+              "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+            },
+            "handle": "@SirIsaac"
+          },
+          "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+          },
+          "created_at": 1461116232227
+        },
+        {
+          "user": {
+            "name": "Descartes",
+            "avatars": {
+              "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+              "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+              "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+            },
+            "handle": "@rd" },
+          "content": {
+            "text": "Je pense , donc je suis"
+          },
+          "created_at": 1461113959088
+        },
+        {
+          "user": {
+            "name": "Johann von Goethe",
+            "avatars": {
+              "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+              "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+              "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+            },
+            "handle": "@johann49"
+          },
+          "content": {
+            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+          },
+          "created_at": 1461113796368
+        }
+      ];
+    
+    //mentor said that given I was behind, ok to use this simpler method below vs. jQuery .add etc.  
+    //this function extracts data from a tweet object, and applies into html container 
     createTweetElement = function(tweetObj) {
         return `<article class="all-tweetsA">
             <header>
@@ -45,10 +77,13 @@
             </footer>`       
     };
     
-    var $tweet = createTweetElement(tweetData);
-    
-    // Test / driver code (temporary)
-    //console.log($tweet); // to see what it looks like
-    $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
- })
+    function renderTweets(tweets) {
+        // loops through tweets from data array, applies html staging, then appends to tweet-container
+        for (let i = 0; i < data.length; i++) {
+            var $tweet = createTweetElement(data[i]);
+            $('#tweet-container').append($tweet);
+        }
+    } 
+    renderTweets(data);
+})
 // Test / driver code (temporary). Eventually will get this from the server.
