d0df5a2e1a3dda9f888d9be84a6c37a7fdb2e51c
Tyler <tyler.tomczyk@gmail.com>
2019-02-21 20:53:05 +0000

    got rid of hard coded data table and refactored a bit in terms of rendering tweets from server

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,66 +6,13 @@
 
  $('document').ready(function(e){
     
-    const data = [
-        {
-          "user": {
-            "name": "Newton",
-            "avatars": {
-              "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-              "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-              "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-            },
-            "handle": "@SirIsaac"
-          },
-          "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-          },
-          "created_at": 1461116232227
-        },
-        {
-          "user": {
-            "name": "Descartes",
-            "avatars": {
-              "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-              "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-              "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-            },
-            "handle": "@rd" },
-          "content": {
-            "text": "Je pense , donc je suis"
-          },
-          "created_at": 1461113959088
-        },
-        {
-          "user": {
-            "name": "Johann von Goethe",
-            "avatars": {
-              "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-              "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-              "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-            },
-            "handle": "@johann49"
-          },
-          "content": {
-            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-          },
-          "created_at": 1461113796368
-        }
-      ];
-    
-    //var tweetArray = null;
-
     function loadTweets () {
+    //function to load tweets from server and append/render on tweet-container 
         $.get("/tweets", (tweets) => {
             let tweetArray = tweets;
-            //console.log(tweetArray)
             renderTweets(tweetArray);
         });
     }loadTweets()
-
-    //console.log(loadTweets());
-
-    //console.log(tweetArray)
 
 
     //mentor said that given I was behind, ok to use this simpler method below vs. jQuery .add etc. 
@@ -94,18 +41,12 @@
     
     function renderTweets(tweets) {
         // loops through tweets from data array, applies html staging, then appends to tweet-container
-        
         for (let i = 0; i < tweets.length; i++) {
             var $tweet = createTweetElement(tweets[i]);
             $('#tweet-container').append($tweet);
         }
     } 
     
-
-   
-    
-
-
     $('.new-tweet').on('submit', (event) => {
         event.preventDefault();
         
@@ -115,10 +56,6 @@
         $.post('/tweets', data)
         
     });
-
-   
-
-    
 
 })
 // Test / driver code (temporary). Eventually will get this from the server.
