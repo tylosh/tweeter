ed50bf2a6d200a8b0ca39b8d930c9e75787bcf59
Tyler <tyler.tomczyk@gmail.com>
2019-02-22 06:12:19 +0000

    curl of the localhost 8080/tweets confirmed to work

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
@@ -17,10 +14,15 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets.sort(sortNewestFirst));
       });
+        
     }
 
   };
